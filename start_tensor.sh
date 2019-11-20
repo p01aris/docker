7fdff17d4a6829fadd1f356bd52ca80b6510824a
p01aris <polaris.varlin@gmail.com>
2019-11-20 11:55:52 +0800

    modify start_tensor.sh

--- start_tensor.sh
+++ start_tensor.sh
@@ -1,7 +1,7 @@
 # Set vars
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-DATA_DIR="$HOME/ghost_volume"
-CONTAINER_NAME="ghost_blog"
+DATA_DIR="$HOME"
+CONTAINER_NAME="cuda_lu_tensorflow"
 
 # Stop and remove the existing container if
 # it is already running.
@@ -9,10 +9,10 @@ docker kill $CONTAINER_NAME
 docker rm $CONTAINER_NAME
 
 docker run \
--d \
---restart=always \
--p 127.0.0.1:2368:2368 \
--v $DATA_DIR/content:/var/lib/ghost/content \
--v $DATA_DIR/config.json:/var/lib/ghost/config.production.json \
+--runtime=nvidia \
 --name $CONTAINER_NAME \
+-p 8888:8888 \
+-v $DATA_DIR:/home\
+--privileged=true \
+-it $CONTAINER_NAME \
 bash
